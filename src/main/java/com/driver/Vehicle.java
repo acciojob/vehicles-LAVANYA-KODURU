c25690d2878dd4e012888c3342e0bb7cfef3be1e
Lavanya Lakshmi Koduru <141067145+LAVANYA-KODURU@users.noreply.github.com>
2025-01-19 11:32:32 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -5,11 +5,26 @@ public class Vehicle {
     private String name;
     private int currentSpeed;
     private int currentDirection;
+
+    // Constructor to initialize the vehicle with a name
     public Vehicle(String name) {
         this.name = name;
         this.currentSpeed = 0;
         this.currentDirection = 0;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
     public void steer(int direction) {
         this.currentDirection += direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
@@ -24,17 +39,5 @@ public class Vehicle {
     public void stop() {
         this.currentSpeed = 0;
         System.out.println("stop method called - The vehicle is stopped");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
-    public int getCurrentDirection() {
-        return currentDirection;
     }
 }
